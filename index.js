ea46bfa49177f797af65b43ffbc306c73e926228
Khushter Nizame <66989627+Khushter420@users.noreply.github.com>
2021-03-16 02:09:14 +0530

    add parents and child

--- index.js
+++ index.js
@@ -31,60 +31,6 @@ for(let i = 0 ; i < iter.length ; ++i)
  iter[0].style.color = 'blue';
 
 
-
-/*
-var input = document.querySelector('input');
-input.value = 'Enter Name';
-
-
-
-var titles = document.querySelectorAll('.item');
-console.log(titles);
-titles[1].style.color = 'green';
-
-*/
-
-/* var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
-
-for(let i = 0 ; i < even.length ; ++i)
-{
-   even[i].style.backgroundColor = 'green';
-
-}
-*/
-
-var list = document.querySelector('.login-form');
- 
-/*console.log(list.lastElementChild);
-list.firstElementChild.placeholder = 'username';
-
- // console.log(list.firstChild);
- // console.log(list.childNodes);
-console.log(list.parentElement);
-list.parentElement.style.backgroundColor = 'black';
-
-console.log(list.lastElementChild);
-list.lastElementChild.textContent = 'hello';
-
-console.log(list.nextElementSibling);
- 
-
-
-console.log(list.previousElementSibling);
-
-*/
-var newdiv = document.createElement('div');
-
-/*newdiv.className = 'hello';
-
-newdiv.id = 'helo 1';
-newdiv.setAttribute('title'  , 'hello bBy');
-*/
-// now we create text node
-
-var newdivText = document.createTextNode('KHUSHTER');
-
 //new li element and get it by class name and tag name
 
  /*var ans=document.getElementsByTagName('li');
@@ -94,3 +40,74 @@ var newdivText = document.createTextNode('KHUSHTER');
  var clans=document.getElementsByClassName('itemx');
  clans[0].innerText='Khushter';
  clans[0].style.fontWeight='bold';
+
+// parents and child
+
+var itemList = document.querySelector('#menu');
+// parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+ console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+ itemList.children[1].style.backgroundColor = 'yellow';
+
+// // FirstChild
+// console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1';
+
+
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+ itemList.lastElementChild.textContent = 'Hello 4';
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+ //console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementSibling
+//console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+
+// createElement
+
+// Create a div
+var newDiv =  document.createElement('div');
+
+// Add class
+newDiv.className= 'hello';
+
+// Add id
+newDiv.id = 'hello1';
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+
+// Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('.form');
+var h1 = document.querySelector('.head');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);
