5fbc5882e4ff2b19aa9ff578da031ab8f94d08b3
Khushter Nizame <66989627+Khushter420@users.noreply.github.com>
2021-03-16 03:27:50 +0530

    add delete,submit and filter button

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-/*console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.all);
-console.log(document.links);
-console.log(document.forms);
-console.log(document.images);
-*/
 
 // Getelementbyid
 
@@ -111,3 +102,72 @@ console.log(newDiv);
 
 newDiv.style.fontSize = '30px';
 container.insertBefore(newDiv, h1);
+
+// add delete ,submit and filter button and eventlistner
+
+var form=document.getElementById('addform');
+var itemList=document.getElementById('menu');
+var filter=document.getElementById('filter');
+
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'itembtn';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+//Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
